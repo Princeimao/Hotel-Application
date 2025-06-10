069187b628f7df93c100eb502347ee82f13d4431
Prince Gupta <pg875029436655@gmail.com>
2025-06-10 21:03:26 +0530

    Added Signin route

--- Host_Service/src/controller/host.controller.ts
+++ Host_Service/src/controller/host.controller.ts
@@ -107,7 +107,7 @@ export const signup_verify = async (req: Request, res: Response) => {
       });
     }
 
-    console.log("something went wrong");
+    console.log("something went wrong", error);
     res.status(500).json({
       success: false,
       message: "something went wrong while verifying otp",
@@ -137,7 +137,7 @@ export const hostDetails = async (req: Request, res: Response) => {
       });
     }
 
-    console.log("something went wrong");
+    console.log("something went wrong", error);
     res.status(500).json({
       success: false,
       message: "something went wrong while saving user details",
@@ -217,10 +217,148 @@ export const hostAddress = async (req: Request, res: Response) => {
       });
     }
 
-    console.log("something went wrong");
+    console.log("something went wrong", error);
     res.status(500).json({
       success: false,
       message: "something went wrong while creating host",
+    });
+  }
+};
+
+export const signin = async (req: Request, res: Response) => {
+  try {
+    const { phone } = phoneSchema.parse(req.body);
+    if (!phone) {
+      res.status(400).json({
+        success: false,
+        message: "Phone Number Is Required Field",
+      });
+    }
+
+    const host = await prisma.host.findUnique({
+      where: {
+        phone: phone,
+      },
+    });
+
+    if (!host) {
+      res.status(400).json({
+        success: false,
+        message: "Host Not found",
+      });
+      return;
+    }
+
+    const existingOtp = await redis.get(`phone:${phone}`);
+    if (existingOtp) {
+      res.status(400).json({
+        success: false,
+        message: "Please try after 5 minutes",
+      });
+      return;
+    }
+
+    const otp = generateOTP();
+    await redis.json.set(`phone:${phone}`, "$", {
+      sub: host.id,
+      email: host.email,
+      otp: otp,
+    });
+    await redis.expire(`phone:${phone}`, 300);
+
+    if (process.env.NODE_ENV === "production") {
+      sendOtp(otp, phone); //  TODO: send otp through worker process
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Your otp is ${otp}`,
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(400).json({
+        success: false,
+        error: error,
+      });
+    }
+
+    console.log("something went wrong", error);
+    res.status(500).json({
+      success: false,
+      message: "something went wrong while signin host",
+    });
+  }
+};
+
+export const signin_verify = async (req: Request, res: Response) => {
+  try {
+    const { otp: hostOtp, phone } = signup_verifySchema.parse(req.body);
+
+    //@ts-ignore
+    const [data] = await redis.json.get(`phone:${phone}`, {
+      path: "$",
+    });
+
+    if (!data) {
+      res.status(400).json({
+        success: false,
+        message: "Otp Expired",
+      });
+      return;
+    }
+
+    if (data.otp !== hostOtp) {
+      res.status(400).json({
+        success: false,
+        message: "Incorrect Otp",
+      });
+      return;
+    }
+
+    if (!process.env.JWT_ACCESS_TOKEN_SECRET) {
+      res.status(500).json({
+        success: false,
+        message: "Internal server error: Jwt secret not found",
+      });
+      return;
+    }
+
+    const jti = uuidv4();
+    // TODO: Also Generate Refresh Token For Long Authentication
+    const AccessToken = jwt.sign(
+      {
+        sub: data.sub,
+        email: data.email,
+        phone: phone,
+        jti: jti,
+      },
+      process.env.JWT_ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1h", // change that to env variabel
+      }
+    );
+
+    res.cookie("AccessToken", AccessToken, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 60,
+    });
+
+    res.status(400).json({
+      success: true,
+      message: "Successfull",
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(400).json({
+        success: false,
+        error: error,
+      });
+    }
+
+    console.log("something went wrong", error);
+    res.status(500).json({
+      success: false,
+      message: "something went wrong while verifying otp",
     });
   }
 };
--- Host_Service/src/router/host.route.ts
+++ Host_Service/src/router/host.route.ts
@@ -1,9 +1,20 @@
 import express from "express";
-import { signup, signup_verify } from "../controller/host.controller";
+import {
+  hostAddress,
+  hostDetails,
+  signin,
+  signin_verify,
+  signup,
+  signup_verify,
+} from "../controller/host.controller";
 
 const router = express.Router();
 
 router.route("/signup").post(signup);
 router.route("/signup-verfiy").post(signup_verify);
+router.route("/host-details").post(hostDetails);
+router.route("/host-address").post(hostAddress);
+router.route("/signin").post(signin);
+router.route("/signin-verify").post(signin_verify);
 
 export default router;
