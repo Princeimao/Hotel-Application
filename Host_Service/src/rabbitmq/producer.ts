cb0e515ea4a478a2c7086943524153f96d8b584b
Prince Gupta <pg875029436655@gmail.com>
2025-06-24 23:04:34 +0530

    added dead letter queue and exchange

---
+++ Host_Service/src/rabbitmq/producer.ts
@@ -0,0 +1,35 @@
+import { getRabbitMqChannel } from "../db/rabbitMq.connection";
+
+interface MessageType {
+  hostId: string;
+  roomId: string;
+}
+
+export const producer = async (queue: string, msg: MessageType) => {
+  try {
+    const channel = await getRabbitMqChannel();
+
+    const dlxExchange = "dead-letter-exchange";
+    const dlqQueue = "dead-letter-queue";
+
+    // declare of dead letter queue and exchange
+    await channel.assertExchange(dlxExchange, "direct", { durable: true });
+    await channel.assertQueue(dlqQueue, { durable: true });
+    await channel.bindQueue(dlqQueue, dlxExchange, "");
+
+    // declare of main queue
+    await channel.assertQueue(queue, {
+      durable: true,
+      arguments: {
+        "x-dead-letter-exchange": dlxExchange,
+      },
+    });
+
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
+  } catch (error) {
+    console.log(
+      "something went wrong while publishing queue to queue - host service",
+      error
+    );
+  }
+};
